7d2977449b112840c2bd1dc4be5046adecd9967d
potipot <p.potrykus@toucan-systems.pl>
2020-05-19 10:24:56 +0200

    added data balancing by folder

--- data/split_ds.py
+++ data/split_ds.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 import argparse
 import random
@@ -5,6 +6,7 @@ import random
 parser = argparse.ArgumentParser(description='split_ds')
 parser.add_argument('--path', type=str, help='dataset main dir', required=True)
 parser.add_argument('--split_pct', default=0.8, type=float, help='n_training/n_total ratio')
+parser.add_argument('--folder_max', default=300, type=int, help='maximum number of files taken from each folder')
 args = parser.parse_args()
 
 main_dir = Path(args.path)
@@ -17,13 +19,25 @@ test = open(out_dir/'test.txt', mode='w')
 trainval = open(out_dir/'trainval.txt', mode='w')
 
 # glob provides random walk
+catalogs = defaultdict(lambda : defaultdict(list))
+
+
 for label_file in anno_dir.glob('**/*.xml'):
     # True or False output based on probability
-    relative_path = label_file.relative_to(anno_dir).with_suffix('').as_posix()
+    relative_path = label_file.relative_to(anno_dir)
+    parent = relative_path.parent or 'root'
+    file = (parent/relative_path.stem).as_posix()
     if random.random() < args.split_pct:
-        trainval.write(relative_path + '\n')
+        # trainval.write(relative_path + '\n')
+        catalogs[parent]['trainval'].append(file)
     else:
-        test.write(relative_path + '\n')
+        # test.write(relative_path + '\n')
+        catalogs[parent]['test'].append(file)
 
+for catalog, files in catalogs.items():
+    trainval_files = files['trainval'][:int(args.folder_max*args.split_pct)]
+    test_files = files['test'][:int(args.folder_max*(1-args.split_pct))]
+    trainval.write('\n'.join(trainval_files)+'\n')
+    test.write('\n'.join(test_files)+'\n')
 test.close()
 trainval.close()
